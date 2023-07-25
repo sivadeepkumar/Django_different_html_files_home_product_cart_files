fcb8f7f6a8558a074a9f7f92a5a9088c5b10d17f
sivadeepkumar <sivadeepkumar3@gmail.com>
2023-07-26 01:20:26 +0530

    Django_receipe_form

--- home/views.py
+++ home/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.http import HttpResponse 
 
-def home(response):
-    return render(response,'home.html')
+from home.models import *
+
+def home(request):
+    if request.method == 'POST':
+
+        data = request.POST
+        receipe_name = data.get('receipe-name')
+        receipe_description = data.get('receipe-description')
+        receipe_image = request.FILES.get('receipe-image')
+
+        print(receipe_name,receipe_description,receipe_image)   
 
 
-def product(response):
-    return render(response,'product.html')
+        # Receipe.objects.create(receipe_name = receipe_name,receipe_description = receipe_description,receipe_image = receipe_image)
+        Receipe.objects.create(receipe_name = receipe_name,receipe_description = receipe_description,receipe_image = receipe_image)
+        return redirect('/')
+    return render(request,'home.html')
 
 
-def cart(response):
-    return render(response,'cart.html')
 
 
